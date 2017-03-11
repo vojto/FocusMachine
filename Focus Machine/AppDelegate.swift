94140e2fa569d7a85b63b1b4f064a265285f2022
Vojtech Rinik <vojto@rinik.net>
2017-03-11 15:01:58 +0100

    Initial Commit

---
+++ Focus Machine/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Focus Machine
+//
+//  Created by Vojtech Rinik on 11/03/2017.
+//  Copyright © 2017 Vojtech Rinik. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
